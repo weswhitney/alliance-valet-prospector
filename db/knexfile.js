1531144e9e13a48b7db5c16f1620d3fa4fdd7df7
weswhitney <wesleywhitney@gmail.com>
2018-08-12 17:23:55 -0600

    added prod settings

--- db/knexfile.js
+++ db/knexfile.js
@@ -2,5 +2,9 @@ module.exports = {
     development: {
         client: 'pg',
         connection: 'postgres://localhost/valet-prospector'
+    },
+    production: {
+        connectionString: process.env.DATABASE_URL,
+        ssl: true
     }
 };
