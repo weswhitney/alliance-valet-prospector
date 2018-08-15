057e9f9c81abf9d66a5484114ee14f315bfe4c82
weswhitney <wesleywhitney@gmail.com>
2018-08-15 10:15:10 -0600

    valet stations route

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET all valet stations */
+router.get('/valet-stations', function (req, res, next) {
+    res.send('send valet stations back');
+});
+
 module.exports = router;
