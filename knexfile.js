74825284a5a13c5fe3a1943c14b0eed6aff5c853
weswhitney <wesleywhitney@gmail.com>
2018-08-21 21:10:39 -0600

    setting up prod db

--- knexfile.js
+++ knexfile.js
@@ -25,15 +25,23 @@ module.exports = {
             directory: __dirname + '/db/seeds/development'
         }
     },
+    // production: {
+    //     client: 'pg',
+    //     connectionString: "dbname=d5sc8907bnfcu5 host=ec2-54-227-241-179.compute-1.amazonaws.com port=5432 user=ojgyiflbcrakjj password=ef9b4f50012c428cc9369ba3fbdd49e3f1c5936330fce32fe766a7e872b559d7 sslmode=require",
+    //     ssl: true,
+    //     migrations: {
+    //         directory: __dirname + '/db/migrations'
+    //     },
+    //     seeds: {
+    //         directory: __dirname + '/db/seeds/production'
+    //     }
+    // }
     production: {
-        client: 'pg',
-        connection: process.env.DATABASE_URL,
-        migrations: {
-            directory: __dirname + '/db/migrations'
-        },
-        seeds: {
-            directory: __dirname + '/db/seeds/production'
+        client: 'postgresql',
+        connection: {
+            database: 'postgres://ojgyiflbcrakjj:ef9b4f50012c428cc9369ba3fbdd49e3f1c5936330fce32fe766a7e872b559d7@ec2-54-227-241-179.compute-1.amazonaws.com:5432/d5sc8907bnfcu5',
+            user: 'ojgyiflbcrakjj',
+            password: 'ef9b4f50012c428cc9369ba3fbdd49e3f1c5936330fce32fe766a7e872b559d7'
         }
     }
-
 };
