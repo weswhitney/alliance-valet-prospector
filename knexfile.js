c6cc4f7ca02e95bf67e71f4c044b643eff27d200
weswhitney <wesleywhitney@gmail.com>
2018-08-23 08:04:08 -0600

    connecting to heroku

--- knexfile.js
+++ knexfile.js
@@ -1,10 +1,18 @@
 // the file which contains our various database configurations. Once you have a knexfile.js, you can use the migration tool
 // to create migration files to the specified directory (default migrations). Creating new migration files can be achieved
 // by running: $ knex migrate:make migration_name
-
+require('dotenv').config({path: 'path_to_env_file'});
 
 module.exports = {
-
+    // production: {
+    //     client: 'pg',
+    //     connection: {
+    //         host: process.env.DB_HOST,
+    //         user: process.env.DB_USER,
+    //         password: process.env.DB_PASS,
+    //         database: process.env.DB_NAME
+    //     }
+    // },
     // test: {
     //     client: 'pg',
     //     connection: 'postgres://localhost/valet-prospector',
@@ -25,6 +33,16 @@ module.exports = {
             directory: __dirname + '/db/seeds/development'
         }
     },
+    production: {
+        client: 'pg',
+        connection: process.env.DATABASE_URL,
+        migrations: {
+            directory: __dirname + '/db/migrations',
+        },
+        seeds: {
+            directory: __dirname + '/db/seeds/production',
+        },
+    },
     // production: {
     //     client: 'pg',
     //     connectionString: "dbname=d5sc8907bnfcu5 host=ec2-54-227-241-179.compute-1.amazonaws.com port=5432 user=ojgyiflbcrakjj password=ef9b4f50012c428cc9369ba3fbdd49e3f1c5936330fce32fe766a7e872b559d7 sslmode=require",
@@ -36,12 +54,27 @@ module.exports = {
     //         directory: __dirname + '/db/seeds/production'
     //     }
     // }
-    production: {
-        client: 'postgresql',
-        connection: {
-            database: 'postgres://ojgyiflbcrakjj:ef9b4f50012c428cc9369ba3fbdd49e3f1c5936330fce32fe766a7e872b559d7@ec2-54-227-241-179.compute-1.amazonaws.com:5432/d5sc8907bnfcu5',
-            user: 'ojgyiflbcrakjj',
-            password: 'ef9b4f50012c428cc9369ba3fbdd49e3f1c5936330fce32fe766a7e872b559d7'
-        }
-    }
+    // production: {
+    //     client: 'postgresql',
+    //     connection: {
+    //         database: 'postgres://ojgyiflbcrakjj:ef9b4f50012c428cc9369ba3fbdd49e3f1c5936330fce32fe766a7e872b559d7@ec2-54-227-241-179.compute-1.amazonaws.com:5432/d5sc8907bnfcu5',
+    //         user: 'ojgyiflbcrakjj',
+    //         password: 'ef9b4f50012c428cc9369ba3fbdd49e3f1c5936330fce32fe766a7e872b559d7'
+    //     }
+    // }
+    // production: {
+    //     client: 'pg',
+    //     connectionString: process.env.DATABASE_URL,
+    //     ssl: true
+    // }
+    // production: {
+    //     client: 'pg',
+    //     connectionString: process.env.DATABASE_URL,
+    //     migrations: {
+    //         directory: __dirname + '/db/migrations'
+    //     },
+    //     seeds: {
+    //         directory: __dirname + '/db/seeds/development'
+    //     }
+    // }
 };
