c03c8ec4c5767b29a1095db10e856282c46d6e7d
weswhitney <wesleywhitney@gmail.com>
2018-08-26 14:47:05 -0600

    commented out problematic seed file i don't need

--- db/seeds/production/show_seeds.js
+++ db/seeds/production/show_seeds.js
@@ -1,50 +1,36 @@
-exports.seed = function(knex, Promise) {
-    return knex('stations').del() // Deletes ALL existing entries
-        .then(function() { // Inserts seed entries one by one in series
-            return knex('stations').insert({
-                valet_name: 'awesome valet',
-                valet_phone: '303-123-4567',
-                valet_web: 'www.awesomevalet.com',
-                valet_email: 'info@awesomevalet.com',
-                valet_contact: 'John Doe',
-                establishment_name: 'The Barn Restaurant',
-                establishment_phone: '303-987-4456',
-                establishment_contact: 'Jaime Hernandez',
-                establishment_email: 'jaime@thebarnres.com',
-                num_employees: 2,
-                address: '1234 E. 1st street Denver, CO 80201',
-                comment: 'pretty busy on a Monday night. not sure where they park the cars'
-
-,            });
-        }).then(function () {
-            return knex('stations').insert({
-                valet_name: 'premier valet',
-                valet_phone: '3038473890',
-                valet_web: 'www.premiervalet.com',
-                valet_email: 'sales@premiervalet.com',
-                valet_contact: 'Grant Huthinson',
-                establishment_name: 'The Milk Barrel',
-                establishment_phone: '303-998-6577',
-                establishment_contact: 'Jack Smith',
-                establishment_email: 'jack@themilkbarrel.com',
-                num_employees: 1,
-                address: '1877 17th street Denver, CO 80201',
-                comment: 'This place looks cool, young crowd. Hipster food court.'
-,            });
-        }).then(function () {
-            return knex('stations').insert({
-                valet_name: 'first valet',
-                valet_phone: '7207489940',
-                valet_web: 'www.firstvalet.com',
-                valet_email: 'info@firstvalet.com',
-                valet_contact: 'Kristen Portland',
-                establishment_name: 'Three Dogs Tavern',
-                establishment_phone: '303-390-0883',
-                establishment_contact: 'Parson Gregario',
-                establishment_email: 'pg@threedogs.com',
-                num_employees: 3,
-                address: '145 Gaylord street Denver, CO 80201',
-                comment: 'this place sucks.'
-            });
-        });
-};
+// exports.seed = function(knex, Promise) {
+//     return knex('test-two').del() // Deletes ALL existing entries
+//         .then(function() { // Inserts seed entries one by one in series
+//             return knex('test-two').insert({
+//                 name: 'Suits',
+//                 channel: 'USA Network',
+//                 genre: 'Drama',
+//                 rating: 3,
+//                 explicit: false
+//             });
+//         }).then(function () {
+//             return knex('test-two').insert({
+//                 name: 'Game of Thrones',
+//                 channel: 'HBO',
+//                 genre: 'Fantasy',
+//                 rating: 5,
+//                 explicit: true
+//             });
+//         }).then(function () {
+//             return knex('test-two').insert({
+//                 name: 'South Park',
+//                 channel: 'Comedy Central',
+//                 genre: 'Comedy',
+//                 rating: 4,
+//                 explicit: true
+//             });
+//         }).then(function () {
+//             return knex('test-two').insert({
+//                 name: 'Mad Men',
+//                 channel: 'AMC',
+//                 genre: 'Drama',
+//                 rating: 3,
+//                 explicit: false
+//             });
+//         });
+// };
