79a04ffdd969164a59be6d80429a38d39f9fde79
weswhitney <wesleywhitney@gmail.com>
2018-08-25 19:50:27 -0600

    simplified data

--- db/migrations/20180819192752_valet_stations.js
+++ db/migrations/20180819192752_valet_stations.js
@@ -2,17 +2,15 @@ exports.up = function(knex, Promise) {
     return knex.schema.createTable('stations', function(table){
         table.increments();
         table.string('valet_name');
-        table.integer('valet_phone');
+        table.string('valet_phone');
         table.string('valet_web');
         table.string('valet_email');
         table.string('valet_contact');
         table.string('establishment_name');
-        table.integer('establishment_phone');
+        table.string('establishment_phone');
         table.string('establishment_contact');
         table.string('establishment_email');
         table.integer('num_employees');
-        table.time('time');
-        table.date('date');
         table.string('address');
         table.string('comment');
         // table.string('payment_form'); add this column
